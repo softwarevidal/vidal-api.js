acb025ff04bf162a584100aa8d766a4692ce2c2e
@fbiville <florent.biville@vidal.fr>
2015-03-19 15:33:33 +0100

    Implement POST
               ,'-',
              :-----:
          (''' , - , ''')
          \   ' .  , `  /
           \  '   ^  ? /
            \ `   -  ,'
             `j_ _,'
        ,- -`\ \  /f
      ,-      \_\/_/'-
     ,                 `,
     ,                   ,
          /\          \
    |    /             \   ',
    ,   f  :           :`,  ,
    <...\  ,           : ,- '
    \,,,,\ ;           : j  '
     \    \            :/^^^^'
      \    \            ; ''':
        \   -,         -`.../
         '    - -,`,--`
          \_._'-- '---:

--- README.md
+++ README.md
@@ -10,6 +10,25 @@ To kickstart your project into calling the [Vidal API](https://vidal.3scale.net/
 	var callback = function(xmlDoc) {
 		// awesome callback here
 	};
+
 	VidalApi.get("http://api.vidal.fr/rest/api/molecules?startwith=a", callback);
 ```
 
+## POST requests
+
+ 1. include `vidal-api.js`
+ 2.
+```javascript
+	var callback = function(xmlDoc) {
+		// awesome callback here
+	},
+	contents = "\
+<?xml version='1.0' encoding='UTF-8' standalone='yes'?> \
+<prescription> \
+	<!-- etc etc --> \
+</prescription>";
+		// default payload type is: 'application/x-www-form-urlencoded'
+		payload = PayloadFactory.create(contents, "text/xml");
+
+	VidalApi.post("http://api.vidal.fr/rest/api/alerts", callback, payload);
+```
--- vidal-api.js
+++ vidal-api.js
@@ -1,4 +1,53 @@
 "use strict";
+
+var PayloadFactory = (function(window) {
+	var Payload = function(type, contents) {
+		var _contents = _parse(contents), 
+			_type = type,
+			_encoding = 'UTF-8',
+			_parse = function(type, payload) {
+				var result;
+				if (type !== 'application/x-www-form-urlencoded') {
+					return payload;
+				}
+				for (var property in payload) {
+					if (payload.hasOwnProperty(property)) {
+						result += property + "=" + payload[property] + "&";
+					}
+				}
+				return encodeURI(result.substring(0, result.length - 1));
+			};
+
+		return {
+			size: function() {
+				return Math.max(0, _contents.split(/%..|./).length);
+			},
+			type: function() {
+				return _type;
+			},
+			encoding: function() {
+				return _encoding;
+			},
+			payload: function() {
+				return _contents;
+			}
+		};
+
+	};
+
+	return {
+		create: function(contents, type) {
+			if (typeof contents === 'undefined') {
+				contents = "";
+			}
+			if (typeof type === 'undefined') {
+				type = 'application/x-www-form-urlencoded';
+			}
+			return Payload(contents, type);
+		}
+	}
+})(window);
+
 var VidalApi = (function(window) {
 
 	function _get(uri, callback) {
@@ -17,6 +66,25 @@ var VidalApi = (function(window) {
 		request.send();
 	}
 
+	function _post(uri, callback, payload) {
+		var request = new XMLHttpRequest();
+
+		request.onreadystatechange = function() {
+			if (request.readyState == 4 && request.status == 200) {
+				callback.apply(
+					this,
+					[new DOMParser().parseFromString(request.responseText, "text/xml")]
+				);
+			}
+		}
+		request.open("POST", uri);
+		request.setRequestHeader("Accept", "application/atom+xml;charset=UTF-8");
+		request.setRequestHeader('Content-Type', payload.type());
+		request.setRequestHeader("Content-Length", payload.size());
+		request.setRequestHeader("Content-Encoding", payload.encoding());
+		request.send(payload.payload());
+	}
+
 	return {
 		/**
 		 * Asynchronous GET requests, fetching atom+XML responses
@@ -24,6 +92,7 @@ var VidalApi = (function(window) {
 		 * @param uri: target URI
 		 * @param callback: callback to apply on the resulting XMLDocument
 		 */
-		get: _get 
+		get: _get,
+		post: _post
 	}
 })(window);
